c276ad6735949f36d7713c51785e1cee64a6c82d
krunal <krunal.maru1106@gmail.com>
2022-08-25 17:23:11 +0530

    login

--- blog/crudapp/urls.py
+++ blog/crudapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from crudapp import views
 urlpatterns = [
-    path('', views.home , name='home')
+    path('', views.home , name='home'),
+    path('login/', views.login, name='login'),
+    path('register/', views.registartion, name='register')
 ]
 
--- blog/crudapp/views.py
+++ blog/crudapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def registartion(request):
+    return render(request, 'register.html')
